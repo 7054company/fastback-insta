ba3686b18cdd952dae621061fd54f96f81c0d2d0
7054company <7054company@gmail.com>
2019-11-10 22:20:54 +0530

    Fastback

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'khvlhgud_insta');
+define('DB_PASS', 'xenex123Ab@');
+define('DB_NAME', 'khvlhgud_insta');
+define('TIMEZONE', 'Pacific/Niue');
+define('ENCRYPTION_KEY', '8de0682e4b3633865bc7ccbcdc29defb');
