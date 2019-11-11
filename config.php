0a490142bd52fabab3b78666df9fff6e6ae54e84
7054company <7054company@gmail.com>
2019-11-11 08:21:07 +0530

    fastback

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
-define('DB_HOST', 'localhost');
-define('DB_USER', 'khvlhgud_insta');
-define('DB_PASS', 'xenex123Ab@');
-define('DB_NAME', 'khvlhgud_insta');
+define('DB_HOST', 'sql210.ezyro.com');
+define('DB_USER', 'ezyro_24467847');
+define('DB_PASS', 'cqywgf');
+define('DB_NAME', 'ezyro_24467847_fastback');
 define('TIMEZONE', 'Pacific/Niue');
 define('ENCRYPTION_KEY', '8de0682e4b3633865bc7ccbcdc29defb');
